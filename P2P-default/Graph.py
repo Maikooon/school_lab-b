5c90daa628a141019717990f2a6fb3ebada7a67d
kate0626 <101503385+Maikooon@users.noreply.github.com>
2024-10-24 00:48:54 +0900

    add default
    デフォルトのものを追加
    時間を計測

--- P2P-default/Graph.py
+++ P2P-default/Graph.py
@@ -2,14 +2,11 @@ from Node import *
 import random
 from queue import Queue
 
-
-class Graph:
+class Graph():
     def __init__(self, ADJ, nodes):
-        print("初期化しました")
         self.nodes = dict()
         self.outside_nodes = dict()
         self.all_nodes = dict()
-        self.all_paths = []
 
         for node_id in ADJ.keys():
             self.nodes[node_id] = nodes[node_id]
@@ -17,9 +14,7 @@ class Graph:
             for adjacent_id in ADJ[node_id]:
                 self.nodes[node_id].add_edge(nodes[adjacent_id])
                 if adjacent_id not in ADJ.keys():
-                    self.outside_nodes[adjacent_id] = self.nodes[node_id].adj[
-                        adjacent_id
-                    ]
+                    self.outside_nodes[adjacent_id] = self.nodes[node_id].adj[adjacent_id]
                     self.all_nodes[adjacent_id] = nodes[adjacent_id]
 
         self.node_count = len(self.nodes)
@@ -32,41 +27,21 @@ class Graph:
                 adj[str(node)].add(str(adj_node))
         return str(adj)
 
-    def random_walk(self, source_id, count, alpha=0.2, all_paths=None):
-        print("random_walk-0,2")
+    def random_walk(self, source_id, count, alpha=0.2):
         source_node = self.nodes[source_id]
         executer = source_node.manager
         end_walk = dict()
         escaped_walk = dict()
 
-        # Use passed all_paths if available
-
-        if all_paths is None:
-            self.all_paths = []
-            self.all_paths.append(source_node.id)
-        else:
-            self.all_paths = all_paths
-
-        # 指定された数だけRWする
         for i in range(count):
             current_node = source_node
             while True:
-                # 異なるサーバに移動
                 if current_node.manager != executer:
-                    escaped_walk[current_node.id] = (
-                        escaped_walk.get(current_node.id, 0) + 1
-                    )
+                    escaped_walk[current_node.id] = escaped_walk.get(current_node.id, 0) + 1
                     break
-                # 終了確率より小さいときには終了
                 if random.random() < alpha:
                     end_walk[current_node.id] = end_walk.get(current_node.id, 0) + 1
-                    print("RWが終了しました")
-                    print("終了時のall_paths", self.all_paths)
                     break
                 current_node = current_node.get_random_adjacent()
-                # 通ったノードを追加する
-                self.all_paths.append(current_node.id)
-            print("end_walk", end_walk)
-            print("escaped_walk", escaped_walk)
-            print("all-paths", self.all_paths)
-        return end_walk, escaped_walk, self.all_paths
+
+        return end_walk, escaped_walk
--- P2P-default/User.py
+++ P2P-default/User.py
@@ -1,10 +1,4 @@
-"""
-hostname -I
-python3 -i User.py 10.58.58.97
-"""
-
 from GraphManager import *
-from Graph import *
 import ast
 
 
@@ -12,11 +6,10 @@ class User:
     def __init__(self, ip_addr):
         self.response_queue = Queue()
         self.ip_addr = ip_addr
-        self.port = 10010
+        self.port = 10020
 
     def send_query(self, source_id, count, GM):
         start_time = time.time()  # 計測を開始
-        print("send_query")
         self.response_queue = Queue()
         message = self.create_message(source_id, count, GM)
         context = zmq.Context()
@@ -32,43 +25,26 @@ class User:
         context = zmq.Context()
         socket = context.socket(zmq.PULL)
         socket.bind("tcp://{}:{}".format(self.ip_addr, self.port))
-
         while count < message.count:
-            print("Expected count: ", message.count)
-            print("count: ", count)
             # パケットを受け取るまでブロック
-            try:
-                rtn_bytes = socket.recv()
-                print("Received: ", rtn_bytes)
-
-                # 受信したデータを辞書として評価
-                received_message = ast.literal_eval(rtn_bytes.decode("utf-8"))
-                end_walk = received_message["end_walk"]
-                all_paths = received_message["all_paths"]
-
-                print("End Walk: ", end_walk)
-                print("All Paths: ", all_paths)
-
-                for node_id, val in end_walk.items():
-                    end_count[node_id] = end_count.get(node_id, 0) + val
-                    count += val
-                    print("count: ", count)
-
-            except Exception as e:
-                print("Error receiving data: ", e)
-
-        average_path_length = len(all_paths) / count
-        print("Average path length: ", average_path_length)
-
-        print("Query solved: ", end_count)
+            rtn_bytes = socket.recv()
+            # literal_eval(str): strを評価して代入
+            # e.g., literal_eval('{'a': 1, 'b': 2}') で 辞書が代入される
+            end_walk = ast.literal_eval(rtn_bytes.decode("utf-8"))
+            for node_id, val in end_walk.items():
+                # dict.get(引数, デフォルト値) で引数が存在すればdict[引数]それ以外はデフォルト値を返す
+                end_count[node_id] = end_count.get(node_id, 0) + val
+                count += val
         socket.close()
         context.destroy()
-        end_time = time.time()
+        # print('Query solved: ', end_count)
+        end_time = time.time()  # 計測を終了
         elapsed_time = end_time - start_time
         print("Elapsed time: ", elapsed_time)
 
+        return end_count
+
     def create_message(self, source_id, count, GM):
-        print("create_message")
         return Message(source_id, count, GM, self.ip_addr)
 
 
