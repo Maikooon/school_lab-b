bcaa9b82e5d344ee5359802596afd97f6f25d0f2
maikoon26 <101503385+Maikooon@users.noreply.github.com>
2025-01-29 17:08:20 +0900

    update

--- new-algo/result-1207/plt0121.py
+++ new-algo/result-1207/plt0121.py
@@ -1,14 +1,25 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# 各棒のデータセット（4本分）
-min_values = [0.6, 0.6, 0.809, 0.809]  # default
-mid_values = [0.87, 0.87, 1.04, 1.04]  # 各棒に対する中間値
-max_values = [1.54, 1.02, 1.626, 1.2158]  # 各棒に対する最大値
+# 各棒のデータセット（4本分）#ここの配列のながさを長くしていくことで対応可能
+min_values = [0.6, 0.6, 0.6, 0.6, 0.809, 0.809, 0.809, 0.809]  # default
+mid_values = [0.87, 0.87, 0.87, 0.87, 1.04, 1.04, 1.04, 1.04]  # 各棒に対する中間値
+max_values = [
+    1.54,
+    1.37,
+    1.07,
+    1.02,
+    1.626,
+    1.5674,
+    1.27,
+    1.23,
+]  # 各棒に対する最大値
 
 # 既存、改善後、既存、改善後
 # x 軸の位置
-x_positions = np.array([0, 1, 3, 4])  # 前半2本: 0,1 / 後半2本: 3,4 (間にギャップを設定)
+x_positions = np.array(
+    [0, 1, 2, 3, 5, 6, 7, 8]
+)  # 前半2本: 0,1 / 後半2本: 3,4 (間にギャップを設定)
 
 # カラーマップ（min, mid, max に対応する色）
 colormap = ["#D3D3D3", "#1E3A8A", "#DC2626"]
@@ -52,9 +63,13 @@ plt.xticks(
     x_positions,
     [
         "ca-gqrc-connected [general method]",
-        "[proposal method]",
-        "com-amazon-connected [general method]",
-        "[proposal method]",
+        "[add 'a1' method]",
+        "[add 'a1 + a2' method]",
+        "[add 'a1 + a2 + b' method]",
+        "com amazon connected [general method]",
+        "[add 'a1' method]",
+        "[add 'a1 + a2' method]",
+        "[add 'a1 + a2 + b' method]",
     ],
     rotation=15,
     ha="right",
